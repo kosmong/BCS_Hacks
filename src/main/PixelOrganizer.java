69feab2803c6d878b45cb0119f48c6ccce060f2e
minesher <minesher@students.cs.ubc.ca>
2022-03-26 11:37:34 -0700

    added commented out pseudocode in PixelOrganizer for map. Uncomment when pixel structure defined.

--- src/main/PixelOrganizer.java
+++ src/main/PixelOrganizer.java
@@ -1,9 +1,16 @@
 package main;
 
+import java.awt.image.BufferedImage;
 import java.util.HashMap;
 
 public class PixelOrganizer {
+    private BufferedImage img;
+    private int width = img.getWidth();
+    private int height = img.getHeight();
     private HashMap<Integer, PixelRegion> allPixels;
+
+    // 0 = border. int 1 - 9 = colours
+    private static final int NUMBER_OF_SECTIONS  = 10;
 
     public PixelOrganizer() {
         allPixels = new HashMap<>();
@@ -16,4 +23,24 @@ public class PixelOrganizer {
             allPixels.get(region).addPixel(pixel);
         }
     }
+    // Iterate through the img array, and put it in the map.
+    // right now, it will simply assign all pixels to one region (region 1) if it's not a border
+    // width height pixel TBD
+//    public void assignAllPixels(){
+//        for (int x = 0; x < width; x++) {
+//            for (int y = 0; y < height; y++) {
+//                int section = getSectionForThisPixel(pixel);
+//                allPixels.put(section, pixel);
+//            }
+//        }
+//    }
+//
+//    public int getSectionForThisPixel(pixel){
+//        if (pixel == black){
+//            return 0;
+//        } else {
+//            return 1;
+//        }
+//    }
+
 }
